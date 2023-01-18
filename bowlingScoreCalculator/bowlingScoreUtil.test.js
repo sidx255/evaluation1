12214c5c6a99ac051d963e092778fa4dcde20dbe
sidx255 <siddharth190820@gmail.com>
2023-01-18 16:26:44 +0530

    chore: add all testcases

--- bowlingScoreCalculator/bowlingScoreUtil.test.js
+++ bowlingScoreCalculator/bowlingScoreUtil.test.js
@@ -40,5 +40,24 @@ describe("Bowling Game Utilities", () =>
       const result = currentScore([1, 1, 2, 3, 4, 5, 4, 5, 5, 3, 3, 2, 3, 4, 5, 5, 3, 7, 10, 10, 4]);
       expect(result).toEqual(102);
     });
+
+    it('should return score when input consist of strike frames', () => {
+      const result = currentScore([10, 10 ,10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+      ]);
+      expect(result).toEqual(300);
+    });
+    it('should return score when input consist of strike frames', () => {
+      const result = currentScore([3, 5, 1 ,5 ,7 ,1 ,10, 1, 6, 10, 6, 2, 1, 2, 0, 5 ,8 ,1]);
+      expect(result).toEqual(89);
+    });
+    it('should return score when input consist of strike frames', () => {
+      const result = currentScore([9, 1, 5, 0, 3, 0, 8, 1, 6, 4, 7, 2, 7, 1, 6, 3, 10 ,4 ,4]);
+      expect(result).toEqual(101);
+    });
+
   })
 );
+
+// 10, 10 ,10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+// 3, 5, 1 ,5 ,7 ,1 ,10, 1, 6, 10, 6, 2, 1, 2, 0, 5 ,8 ,1
+// 9, 1, 5, 0, 3, 0, 8, 1, 6, 4, 7, 2, 7, 1, 6, 3, 10 ,4 ,4
