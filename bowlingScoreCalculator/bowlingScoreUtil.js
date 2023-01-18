3b0abc3970cb166ef710e2322b672f4acf769661
sidx255 <siddharth190820@gmail.com>
2023-01-18 12:45:34 +0530

    feat: add error handling to score calculator

--- bowlingScoreCalculator/bowlingScoreUtil.js
+++ bowlingScoreCalculator/bowlingScoreUtil.js
@@ -1,43 +1,3 @@
-//let rolls = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-function checkForErrors(rolls, fromScore) {
-    const maxFrameScore = 10
-    const lastFrame = 9
-    const bonusFrame = 10
-    let firstRoll = 0
-    let secondRoll = 0
-    let game = []
-    // check to see if we have any rolls
-    if (fromScore == true && rolls.length == 0) {
-        throw 'Score cannot be taken until the end of the game'
-    }
-
-    // check for frame errors
-    if (fromScore == false) {
-        for (let frame = 0; frame < game.length; frame++) {
-            // two rolls in a frame must be <= 10
-            [firstRoll, secondRoll] = game[frame]
-            if (firstRoll + secondRoll > 10) {
-                throw 'Pin count exceeds pins on the lane'
-            }
-        }
-    } // end of check for frame errors for
-    // Are we trying to roll another ball after we have 10 frames with no strike or spare in frame 10
-    if (fromScore == false && game.length > 10) {
-        //console.log(game)
-        //console.log(`Frame ${lastFrame} Last Frame Roll 1 ${game[lastFrame][0]} Last Frame Roll 2 ${game[lastFrame][1]}`)
-        if (game[lastFrame][0] + game[lastFrame][1] < 10) {
-            throw 'Cannot roll after game is over'
-        }
-    }
-    // do we have less than 10 frames
-    if (fromScore == true && game.length < 10) {
-        throw 'Score cannot be taken until the end of the game'
-    }
-    // check to see if we have enough frames
-    if (fromScore == true && game.length < 10) {
-        throw 'Score cannot be taken until the end of the game'
-    }
-}
 
 const currentScore = rolls => {
 
@@ -68,6 +28,7 @@ const currentScore = rolls => {
             frameStart = true;
         }
     }
+    if (isNan.currentScore) throw new error 
     return currentScore;
 }
 
