b74647402da68ba26512058385f35b6dbc183ae4
sidx255 <siddharth190820@gmail.com>
2023-01-18 13:57:28 +0530

    feat: format code using eslint

--- bowlingScoreCalculator/bowlingScoreUtil.js
+++ bowlingScoreCalculator/bowlingScoreUtil.js
@@ -1,38 +1,38 @@
 //let rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 3, 3, 3, 3, 3]
 const currentScore = rolls => {
 
-    const strike = 10;
-    const spare = 10;
-    let frameStart = true;
-    let currentScore = 0;
-    let length = 20;
-    if(rolls.length > 21){
-        throw new Error("Frame size is not 10");
+  const strike = 10;
+  const spare = 10;
+  let frameStart = true;
+  let currentScore = 0;
+  let length = 20;
+  if(rolls.length > 21){
+    throw new Error("Frame size is not 10");
+  }
+  for (let roll = 0; roll < length; roll++) {
+    if (frameStart == true) {
+      if (rolls[roll] == strike) {
+        currentScore += (strike + rolls[roll + 1] + rolls[roll + 2]);
+        length--;
+      } else {
+        currentScore += rolls[roll];
+        frameStart = false;
+      }
+    } else {
+      // we are at the end of a frame, do we have a spare
+      if (rolls[roll] + rolls[roll - 1] == spare) {
+        // spare so add this roll + next roll
+        // we don't add 10 because the first ball was already added at the frame start
+        currentScore += rolls[roll] + rolls[roll + 1];
+      } else {
+        currentScore += rolls[roll];
+      }
+      frameStart = true;
     }
-    for (let roll = 0; roll < length; roll++) {
-        if (frameStart == true) {
-            if (rolls[roll] == strike) {
-                currentScore += (strike + rolls[roll + 1] + rolls[roll + 2])
-                length--
-            } else {
-                currentScore += rolls[roll]
-                frameStart = false
-            }
-        } else {
-            // we are at the end of a frame, do we have a spare
-            if (rolls[roll] + rolls[roll - 1] == spare) {
-                // spare so add this roll + next roll
-                // we don't add 10 because the first ball was already added at the frame start
-                currentScore += rolls[roll] + rolls[roll + 1]
-            } else {
-                currentScore += rolls[roll]
-            }
-            frameStart = true;
-        }
-    }
-    //if (isNan.currentScore) throw new error ("Frames are not 10"); 
-    return currentScore;
-}
+  }
+  //if (isNan.currentScore) throw new error ("Frames are not 10"); 
+  return currentScore;
+};
 //console.log(currentScore(rolls));
 
 module.exports = { currentScore };
--- bowlingScoreCalculator/bowlingScoreUtil.test.js
+++ bowlingScoreCalculator/bowlingScoreUtil.test.js
@@ -1,31 +1,31 @@
 
 
-const { currentScore } = require('./bowlingScoreUtil');
+const { currentScore } = require("./bowlingScoreUtil");
 
-describe('Bowling Game Utilities', () =>
-    describe('Calculating Current Score', () => {
-        it('Should throw an error when frame size is not 10', () => {
-            expect(()=>
-            {
-                currentScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 3, 3, 3, 3, 3])
-            }).toThrow("Frame size is not 10");
-        }),
+describe("Bowling Game Utilities", () =>
+  describe("Calculating Current Score", () => {
+    it("Should throw an error when frame size is not 10", () => {
+      expect(()=>
+      {
+        currentScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 3, 3, 3, 3, 3]);
+      }).toThrow("Frame size is not 10");
+    }),
 
-        it('Should return 16 when rolls are [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]', () => {
-            const result = currentScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-            expect(result).toEqual(16);
-        }),
+    it("Should return 16 when rolls are [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]", () => {
+      const result = currentScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      expect(result).toEqual(16);
+    }),
 
-        it('Should return 30 when rolls are [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]', () =>
-        {
-            const result = currentScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]);
-            expect(result).toEqual(30);
-        }),
+    it("Should return 30 when rolls are [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]", () =>
+    {
+      const result = currentScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]);
+      expect(result).toEqual(30);
+    }),
 
-        it('Should return 90 when rolls are [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]', () =>
-        {
-            const result = currentScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
-            expect(result).toEqual(90);
-        });
-    })
-)
+    it("Should return 90 when rolls are [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]", () =>
+    {
+      const result = currentScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+      expect(result).toEqual(90);
+    });
+  })
+);
